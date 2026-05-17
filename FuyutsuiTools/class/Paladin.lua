74030e1b94777d17b2477c091762c125a10fb026
netlaoer <69303073+netlaoer@users.noreply.github.com>
2026-05-17 18:51:46 +0800

    Add files via upload

--- FuyutsuiTools/class/Paladin.lua
+++ FuyutsuiTools/class/Paladin.lua
@@ -1,11 +1,6 @@
 if UnitClassBase("player") ~= "PALADIN" then return end
 
--- 为所有圣骑士专精添加驱散开关像素块
-if Fuyutsui.ClassBlocks then
-    local steps = { [1] = 48, [2] = 51, [3] = 48 }
-    for spec = 1, 3 do
-        if Fuyutsui.ClassBlocks[spec] then
-            Fuyutsui.ClassBlocks[spec][steps[spec]] = { type = "block", name = "驱散开关" }
-        end
-    end
+-- 仅神圣专精添加驱散开关像素块
+if Fuyutsui.ClassBlocks and Fuyutsui.ClassBlocks[1] then
+    Fuyutsui.ClassBlocks[1][48] = { type = "block", name = "驱散开关" }
 end
